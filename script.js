0796b4b15e4107755c92c91c397741d526f65b3c
Abhyuday911 <abhyuday911@gmail.com>
2022-05-16 17:03:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("#on").addEventListener("click", function () {
+    document.querySelector("#circle").style.backgroundColor = "rgb(222, 236, 95)";
+})
+document.querySelector("#off").addEventListener("click", function () {
+    document.querySelector("#circle").style.backgroundColor = "transparent";
+})
+//FORMAT
+//Select karo jiss kam karwana hai.uspe event lgao("type",function){fir
+//function ke andar select karo jispe action hoga.htmlchange/css chang}
